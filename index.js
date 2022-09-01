5017fee31d79d3381a7dc1925d2de8aeee052181
aprilkrg <aprilkrgonzales@gmail.com>
2022-09-01 15:22:34 -0700

    create routes for home, about, blog that res.sends msg

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+
+const PORT = 3000
+
+// ROUTES
+// home route
+// HTTP verb = GET   URL pattern = "localhost:3000/"
+app.get("/", (request, response) => {
+    response.send("This is the Home Page!")
+})
+
+// HTTP verb = GET   URL pattern = "localhost:3000/about"
+app.get("/about", (req, res) => {
+    res.send("Some stuff about me will go here.")
+})
+
+// HTTP verb = GET   URL pattern = "localhost:3000/blog"
+app.get("/blog", (req, res) => {
+    res.send("A directory of all my blog posts will go here.")
+})
+
+// LISTENER GOES AT THE BOTTOM
+app.listen(PORT, () => {
+    console.log(`The bird channel is showing on port ${PORT}`)
+})
