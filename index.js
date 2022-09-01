ca66b7801f5af4cde67375862cbe09f8068f4ff4
aprilkrg <aprilkrgonzales@gmail.com>
2022-09-01 15:49:56 -0700

    complete views lesson part 1

--- index.js
+++ index.js
@@ -15,12 +15,17 @@ app.get("/", (request, response) => {
 
 // HTTP verb = GET   URL pattern = "localhost:3000/about"
 app.get("/about", (req, res) => {
-    res.send("Some stuff about me will go here.")
+    // SENDS HTML
+    // res.send("Some stuff about me will go here.")
+    // SENDS HTML FILE
+    res.sendFile(__dirname+"/views/about.html")
 })
 
 // HTTP verb = GET   URL pattern = "localhost:3000/blog"
 app.get("/blog", (req, res) => {
-    res.send("A directory of all my blog posts will go here.")
+    // res.send("A directory of all my blog posts will go here.")
+    res.sendFile(__dirname+"/views/blog-directory.html")
+
 })
 
 // LISTENER GOES AT THE BOTTOM
